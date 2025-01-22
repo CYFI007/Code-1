23c34f1ecc6414b70893f132448e0131894c6b01
cyfi007 <tup59563@temple.edu>
2025-01-22 10:44:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 let song = {
     title: "Apple",
     album: "Brat"
+    <li><code>2023</code>
+    <li><code>isAvailable</code> (type: boolean)</li>
+
 }
 
 //↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
